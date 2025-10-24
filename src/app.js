ed678c28b5ee81c128439d27461099cede6a272b
EBastidas <114364709+EmilyBastidas@users.noreply.github.com>
2025-10-24 21:00:40 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
+/* import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+*/
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function (generatorDom) {
+
+  let pronombre = ['nuestro', 'los', 'las', 'sus'];
+  let adjetivo = ['mejores', 'grandes'];
+  let sustantivo = ['ideas', 'corredor'];
+  let dominios = ['.com'];
+
+
+  for (let p = 0; p < pronombre.length; p++) {
+    for (let a = 0; a < adjetivo.length; a++) {
+      for (let s = 0; s < sustantivo.length; s++) {
+
+        console.log(pronombre[p] + adjetivo[a] + sustantivo[s] + dominios[0]);
+      }
+    }
+  }
+
 };
+
